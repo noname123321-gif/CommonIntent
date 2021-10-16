d36ac8cd30bb8fe246e039ae62c312db7fd19a60
Lê Huy <62213854+huyle-itus@users.noreply.github.com>
2021-10-17 00:22:33 +0700

    Camera-Intent

--- app/src/main/java/com/example/commonintent/NoteActivity.java
+++ app/src/main/java/com/example/commonintent/NoteActivity.java
@@ -1,15 +1,44 @@
 package com.example.commonintent;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoteActivity extends AppCompatActivity {
+    Button btnCamera;
+    ImageView imageView;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-        getSupportActionBar().setTitle("Note Intent");
+        getSupportActionBar().setTitle("Camera Intent");
+
+        btnCamera = (Button) findViewById(R.id.cameraButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
